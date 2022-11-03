b6e130c439f86e80888cbbe8167711f6fab3d980
Krylova Nataliya <natalinebo2014@yandex.ru>
2022-11-03 15:29:21 +0100

    add daily forecast change Farenheit/Celcius

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const cityForm = document.querySelector("#city-form");
 const celcius = document.querySelector(".celcius");
 const farenheit = document.querySelector(".farenheit");
 const currentTemperature = document.querySelector(".current-temperature");
+const currentFeelTemperature = document.querySelector(
+  ".current-feel-temperature"
+);
+const currentWeatherDescription = document.querySelector(
+  ".current-weather-description"
+);
 const forecast = document.querySelector(".forecast");
 const weekDays = [
   "Sunday",
@@ -17,6 +23,7 @@ const weekDays = [
   "Friday",
   "Saturday",
 ];
+let isCelcius = true;
 
 /*-----------------date-------------------- */
 
@@ -51,53 +58,13 @@ function setZero(data) {
 
 cityForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("?");
   city.innerHTML = cityInput.value;
 });
 
 /*------------------celcius-farenheit--------------- */
 
-let isCelcius = true;
-celcius.addEventListener("click", function () {
-  if (isCelcius === true) {
-    console.log(isCelcius);
-    return;
-  } else {
-    console.log(isCelcius);
-    let celciusString = Math.round(
-      (Number(currentTemperature.innerHTML) - 32) / 1.8
-    );
-    currentTemperature.innerHTML = celciusString;
-    celcius.classList.add("active-degree");
-    farenheit.classList.remove("active-degree");
-    isCelcius = true;
-  }
-});
-
-farenheit.addEventListener("click", function () {
-  if (isCelcius === true) {
-    console.log(isCelcius);
-    let farenheitString = Math.round(
-      Number(currentTemperature.innerHTML) * 1.8 + 32
-    );
-    currentTemperature.innerHTML = farenheitString;
-    celcius.classList.remove("active-degree");
-    farenheit.classList.add("active-degree");
-    isCelcius = false;
-  } else {
-    console.log(isCelcius);
-    return;
-  }
-});
-
 /*------------------------display current weather----------------------------- */
 
-let currentWeatherDescription = document.querySelector(
-  ".current-weather-description"
-);
-let currentFeelTemperature = document.querySelector(
-  ".current-feel-temperature"
-);
 let currentWind = document.querySelector(".current-wind");
 let currentHumidity = document.querySelector(".current-humidity");
 let currentSunrise = document.querySelector(".current-sunrise");
@@ -117,6 +84,7 @@ function submitSearch(event) {
   event.preventDefault();
   let cityName = cityInput.value;
   setCity(cityName);
+  forecast.innerHTML = "";
 }
 
 cityForm.addEventListener("submit", submitSearch);
@@ -133,18 +101,13 @@ function formatDay(timeSpamp) {
   let day = date.getDay();
   return weekDays[day];
 }
-console.log(formatDay(1667732400));
 
 function getForecast(coordinates) {
-  console.log(coordinates.lat);
-  let apiKey = "5c08670149a0b1a4dc7a372a3d5e5333";
   const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
 function showWeather(response) {
-  console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
   let weatherDesc = response.data.weather[0].description;
   let feelTemperature = Math.round(response.data.main.feels_like);
@@ -169,10 +132,7 @@ function showWeather(response) {
 function setPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  console.log(latitude);
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  console.log(url);
-
   axios.get(url).then(showWeather);
 }
 
@@ -186,12 +146,14 @@ currentGeolocation.addEventListener("click", getCurrentLocation);
 /*----------------------display week days forecast---------------------- */
 
 function displayForecast(response) {
-  console.log(response.data.daily);
   const forecastArr = response.data.daily;
   forecastArr.forEach((day, index) => {
     if (index > 0 && index < 7) {
       let dayForecast = document.createElement("div");
       dayForecast.classList.add("day-forecast", "col");
+      let minTempreture = Math.round(day.temp.min);
+      let maxTempreture = Math.round(day.temp.max);
+
       dayForecast.innerHTML = `<div class="week-day-min">${formatDay(
         day.dt
       )}</div>
@@ -199,10 +161,64 @@ function displayForecast(response) {
               day.weather[0].icon
             }@2x.png" alt="${day.weather[0].description}"></div>
             <div class="temperature-min">
-              <span class="temp-max">${Math.round(day.temp.min)}°</span>
-              <span class="temp-max">${Math.round(day.temp.max)}°</span>
+              <span class="temp-min">${minTempreture}°</span>
+              <span class="temp-max">${maxTempreture}°</span>
             </div>`;
       forecast.append(dayForecast);
+    }
+  });
+
+  celcius.addEventListener("click", function () {
+    if (isCelcius === true) {
+      return;
+    } else {
+      currentTemperature.innerHTML = Math.round(
+        (Number(currentTemperature.innerHTML) - 32) / 1.8
+      );
+      currentFeelTemperature.innerHTML = Math.round(
+        (Number(currentFeelTemperature.innerHTML) - 32) / 1.8
+      );
+
+      let tempMin = document.querySelectorAll(".temp-min");
+      tempMin.forEach((item) => {
+        item.innerHTML =
+          Math.round((parseInt(item.innerHTML) - 32) / 1.8) + "°";
+      });
+
+      let tempMax = document.querySelectorAll(".temp-max");
+      tempMax.forEach((item) => {
+        item.innerHTML =
+          Math.round((parseInt(item.innerHTML) - 32) / 1.8) + "°";
+      });
+      celcius.classList.add("active-degree");
+      farenheit.classList.remove("active-degree");
+      isCelcius = true;
+    }
+  });
+
+  farenheit.addEventListener("click", function () {
+    if (isCelcius === true) {
+      currentTemperature.innerHTML = Math.round(
+        Number(currentTemperature.innerHTML) * 1.8 + 32
+      );
+      currentFeelTemperature.innerHTML = Math.round(
+        Number(currentFeelTemperature.innerHTML) * 1.8 + 32
+      );
+      let tempMin = document.querySelectorAll(".temp-min");
+      tempMin.forEach((item) => {
+        item.innerHTML = Math.round(parseInt(item.innerHTML) * 1.8 + 32) + "°";
+      });
+
+      let tempMax = document.querySelectorAll(".temp-max");
+      tempMax.forEach((item) => {
+        item.innerHTML = Math.round(parseInt(item.innerHTML) * 1.8 + 32) + "°";
+      });
+
+      celcius.classList.remove("active-degree");
+      farenheit.classList.add("active-degree");
+      isCelcius = false;
+    } else {
+      return;
     }
   });
 }
